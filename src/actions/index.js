8566b4ba067a0fe239fb06dcc48cf69236d2148b
Noel Gonzales <noelalangonzales@gmail.com>
2016-09-12 15:38:12 -0700

    Initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = "4aee2405fa73ca0a70fbf40a1e86b2a4";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url)
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
